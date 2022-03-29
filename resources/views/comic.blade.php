e0272c177ed72708434bb40a17655bc14898166a
antoniogiachin <giachinantonio@outlook.com>
2022-03-29 15:50:01 +0200

    stilizzazione overview

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -6,9 +6,32 @@
 @section('content')
     {{-- qui va main --}}
     <main id="comic-page">
+        <div class="divider"></div>
         <div class="container">
-            <div class="divider"></div>
-            <h1>{{$selectedComic['title']}}</h1>
+            <div class="overview">
+               <div class="overview-left">
+                   <h1>{{$selectedComic['title']}}</h1>
+                   <div class="price">
+                       <div class="price-left">
+                           <span>Us Price: {{$selectedComic['price']}}</span>
+                           <span class="txt-uc">Available</span>
+                       </div>
+                       <div class="price-right">
+                           <select name="check-av" id="av-check">
+                               <option value="">Check Availability</option>
+                           </select>
+                       </div>
+                    </div>
+                    <p>{{$selectedComic['description']}}</p>
+                </div> 
+                <div class="overview-right">
+                    <span class="txt-uc">Advertisement</span>
+                    <img src="{{$selectedComic['thumb']}}" alt="{{$selectedComic['title']}}">
+                </div>
+            </div>
+            <div class="desc">
+                
+            </div>
         </div>
     </main>
 @endsection
